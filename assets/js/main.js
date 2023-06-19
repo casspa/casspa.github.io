cd09cd963faeae860264d3a4e4ca4aa4cbc262ff
casspa <47937569+casspa@users.noreply.github.com>
2023-06-19 18:27:24 +0930

    Website updated to show current work portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+    interval: 100,
+})
+
+sr.reveal('.home-section')
+sr.reveal('.portfolio-section')
+sr.reveal('.about-section')
+sr.reveal('.project-description-section')
+
+
